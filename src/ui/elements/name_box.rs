de00621c236da68a953c0a2f9df988661ef1163a
new-years-eve <new-years-evening@proton.me>
2026-02-08 22:22:00 +0100

    Don't mark name box as modified if an unmodified name is entered

--- src/ui/elements/name_box.rs
+++ src/ui/elements/name_box.rs
@@ -35,8 +35,8 @@ impl NameBox {
                 if response.clicked()
                     || (edit_response.lost_focus() && ui.input(|i| i.key_pressed(egui::Key::Enter)))
                 {
+                    cheese_response.modified = *text != self.edit_content;
                     *text = std::mem::take(&mut self.edit_content);
-                    cheese_response.modified = true;
                     self.editing = false;
                 }
                 let response = ui.button("❌");
