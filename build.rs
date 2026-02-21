f435edeec7fc56e68683a0f4cc9c81bf7e8797cb
Brie <brie@brie.gay>
2026-02-21 13:37:53 -0500

    Set icon on windows

---
+++ build.rs
@@ -0,0 +1,12 @@
+fn main() {
+    if cfg!(target_os = "windows") {
+        let mut res = winres::WindowsResource::new();
+        res.set_icon("resources/cheese-paper-icon.ico")
+            .set("FileDescription", "cheese-paper text editor")
+            .set("LegalCopyright", "License: GPLv3 or later");
+        if let Err(err) = res.compile() {
+            println!("Could not compile resources: {err}");
+            std::process::exit(1);
+        }
+    }
+}
