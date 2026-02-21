f61f8854c2149a12d9731495d3471a3140c7e1b1
Brie <brie@brie.gay>
2026-02-21 13:56:58 -0500

    Only compile windows resources on windows
    This is what I was trying to do in the last commit, but done
    successfully this time

--- build.rs
+++ build.rs
@@ -1,5 +1,6 @@
 fn main() {
-    if cfg!(target_os = "windows") {
+    #[cfg(target_os = "windows")]
+    {
         let mut res = winres::WindowsResource::new();
         res.set_icon("resources/cheese-paper-icon.ico")
             .set("FileDescription", "cheese-paper text editor")
