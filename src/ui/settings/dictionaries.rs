34080483ae702e249ead7f0f1ca048ce4294e39d
Brie <brie@brie.gay>
2026-06-07 14:52:05 -0400

    Look for dictionaries in `XDG_DATA_DIRS` (#304)

--- src/ui/settings/dictionaries.rs
+++ src/ui/settings/dictionaries.rs
@@ -1,4 +1,8 @@
-use std::{env, path::PathBuf};
+use std::{
+    collections::HashSet,
+    env,
+    path::{Path, PathBuf},
+};
 
 use super::SettingsData;
 use crate::ui::prelude::*;
@@ -64,7 +68,7 @@ impl SettingsData {
         // For each dictionary name, we will only keep the first dictionary we load
         // The order in which search locations are in this list determines
         // which dictionary we pick in priority if there are mulitple
-        let mut dict_search_paths = Vec::new();
+        let mut dict_search_paths = HashSet::new();
 
         let mut app_dict_folder_path = self.settings_path.clone();
         app_dict_folder_path.push(PathBuf::from("dictionaries"));
@@ -73,20 +77,41 @@ impl SettingsData {
             std::fs::create_dir_all(&app_dict_folder_path)?;
         }
 
-        dict_search_paths.push(app_dict_folder_path);
+        dict_search_paths.insert(app_dict_folder_path);
 
         if env::consts::OS == "linux" {
-            dict_search_paths.push(PathBuf::from("/usr/share/hunspell/"));
+            let parent_search_dirs = if let Ok(env_data_dirs) = env::var("XDG_DATA_DIRS")
+                && !env_data_dirs.is_empty()
+            {
+                env_data_dirs
+            } else {
+                String::from("/usr/local/share/:/usr/share/")
+            };
+
+            for parent_search_dir in parent_search_dirs.split(":") {
+                let dict_dir = Path::new(parent_search_dir).join("hunspell");
+                if dict_dir.exists() {
+                    dict_search_paths.insert(dict_dir);
+                }
+            }
+
+            let data_home = env::var("XDG_DATA_HOME")
+                .map(PathBuf::from)
+                .unwrap_or_else(|_| env::home_dir().unwrap().join(".local/share"));
+            let home_dict_dir = data_home.join("hunspell");
+            if home_dict_dir.exists() {
+                dict_search_paths.insert(home_dict_dir);
+            }
         } else if env::consts::OS == "macos"
             && let Ok(exe_path) = std::env::current_exe()
             && let Some(exe_folder) = exe_path.parent()
         {
-            dict_search_paths.push(exe_folder.join("../Resources/resources/spellcheck/en_US/"));
+            dict_search_paths.insert(exe_folder.join("../Resources/resources/spellcheck/en_US/"));
         } else if env::consts::OS == "windows"
             && let Ok(exe_path) = std::env::current_exe()
             && let Some(exe_folder) = exe_path.parent()
         {
-            dict_search_paths.push(exe_folder.join("../resources/spellcheck/en_US/"));
+            dict_search_paths.insert(exe_folder.join("../resources/spellcheck/en_US/"));
         }
 
         for search_path in &dict_search_paths {
